2ae501087b4835346afd0a05e8dc8bd9bf79a6cc
hq1350 <user1350>
2021-08-11 10:47:01 +0800

    test

---
+++ basics/list/ArrayListTest.java
@@ -0,0 +1,15 @@
+package basics.list;
+
+import java.util.ArrayList;
+
+/**
+ * @Author: cookpotatoe
+ * @Date: 2021/8/11 10:37
+ * @Description:arrayList的各种用法
+ */
+
+public class ArrayListTest {
+    public ArrayList arrayList = new ArrayList();
+
+}
+
